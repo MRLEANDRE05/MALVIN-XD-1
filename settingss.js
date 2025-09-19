467921320fdf7801bbce909b57af2e059f8e370f
MRLEANDRE05 <celestinmboussa@gmail.com>
2025-09-19 10:06:13 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~oqkRlCrZ#14ZY1GDl8DC0GTupv6EoqQP045sPlEQzXio_4r_R8n4",  // add your Session Id here
   
-  OWNER_NUMBER: "263776388689", // put your phone number here
+  OWNER_NUMBER: "237658871046", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
